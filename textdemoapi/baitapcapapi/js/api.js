6eb1b07d82a20a3833237cf83bbcb1c1fed0e122
hyynee <122151727+hyynee@users.noreply.github.com>
2023-04-01 22:07:16 +0700

    Add files via upload

---
+++ textdemoapi/baitapcapapi/js/api.js
@@ -0,0 +1,61 @@
+function renderTable(arr) {
+  var htmlString = "";
+  for (var index = 0; index < arr.length; index++) {
+    var dg = arr[index];
+    htmlString += `
+      <div class="col  col-sm-6 col-lg-4 col-12 p-2">
+      <div class="card mx-auto">
+      <div class="card__heart">
+          <i class="fa fa-heart"></i>
+      </div>
+      <div class="card_cart">
+          <i class="fa fa-shopping-cart"></i>
+      </div>
+      <div class="card_img">
+          <img src="${dg.image}" alt="">
+      </div>
+      <div class="card-body">
+      <div class="card__title">
+      <h4 class="fw-semibold">${dg.name}</h4> <br/>
+      <i style="font-size:12px">The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.</i>
+      </div>
+      <div  class="d-flex start-item justify-content-center align-items-center"
+      style="color: #f9d412">
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+      </div>
+      </div>
+      <div class="card-bottom">
+      <div class="rating-button">
+      <div class="card_action">
+      <a class="btn-Buy" href="../views/detail.html?id=${dg.id}"> Buy Now </a>
+         <button class="price">$${dg.price}</button>
+      </div>
+      </div>
+      </div>
+  </div>
+    </div>
+      `;
+  }
+  document.querySelector("#tbody .row").innerHTML = htmlString;
+  return htmlString;
+}
+
+function layDanhSach() {
+  var promise = axios({
+    url: "https://shop.cyberlearn.vn/api/Product",
+    method: "GET",
+    responseType: "json",
+  });
+  promise.then(function (res) {
+    console.log(res.data.content);
+    renderTable(res.data.content);
+  });
+  // promise.catch(function (err) {
+  //   console.log(err);
+  // });
+}
+layDanhSach();
---
+++ textdemoapi/baitapcapapi/js/detail.js
@@ -0,0 +1,82 @@
+window.onload = async function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get("id");
+  console.log("params", myParam);
+  /* call api load len giao dien */
+  try {
+    let res = await axios({
+      url: `https://shop.cyberlearn.vn/api/Product/getbyid?id=${myParam}`,
+      method: "GET",
+      responseType: "json",
+    });
+    let newArr = [];
+    newArr.push(res.data.content);
+    console.log(res.data.content);
+    renderTable([res.data.content]);
+  } catch (err) {
+    console.log(err.data.content);
+  }
+};
+// này là lấy ra nguyên cái mảng giày từ 1-19
+function renderTable(arr) {
+  var htmlString = "";
+  for (var index = 0; index < arr.length; index++) {
+    var gd = arr[index];
+    htmlString += `
+     <div class="d-flex justify-content-between avable ${gd.id}">
+     <div class="card">
+     <div class="img1">
+       <img src="${gd.image}" alt="..." />
+     </div>
+   </div>
+     <div class="detail-content">
+       <h1 class="title">${gd.name}</h1>
+       <p class="text">
+       ${gd.description}
+       </p>
+       <h3 class="text-success fw-semibold">Avalable size</h3>
+       <div id="sizes" class="mt-2">
+         <button class="btn text-white bg-dark">${gd.size[0]}</button>
+         <button class="btn text-white bg-dark">${gd.size[1]}</button>
+         <button class="btn text-white bg-dark">${gd.size[2]}</button>
+         <button class="btn text-white bg-dark">${gd.size[3]}</button>
+         <button class="btn text-white bg-dark">${gd.size[4]}</button>
+         <button class="btn text-white bg-dark">${gd.size[5]}</button>
+       </div>
+       <p class="display-6 text-danger">$${gd.price}</p>
+       <div class="buttons_added" style="font-size:20px;"> Số Lượng:${gd.quantity} <br>
+         <input
+           class="minus is-form"
+           id="minus"
+           onclick="Minus()"
+           type="button"
+           value="-"
+           style="width: 7%; background-color: cornflowerblue"
+         />
+         <input
+           aria-label="quantity"
+           id="number"
+           class="input-qty"
+           max="Số tối đa"
+           min="Số tối thiểu"
+           name=""
+           type=""
+           value="1"
+           style="width: 7%"
+         />
+         <input
+           class="plus is-form"
+           id="plus"
+           type="button"
+           value="+"
+           style="width: 7%; background-color: cornflowerblue"
+         />
+       </div>
+       <button class="btn-Buy">Add to cart</button>
+     </div>
+     </div> 
+     `;
+  }
+  document.querySelector("#detail #detail__container").innerHTML = htmlString;
+  return htmlString;
+}
